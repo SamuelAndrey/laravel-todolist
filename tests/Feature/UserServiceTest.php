d45011b2fbebe85841a5da69bcb1e7a74526d2d1
samuelandrey <samuelandrey2021@gmail.com>
2024-03-01 17:50:43 +0700

    Implement database to user

--- tests/Feature/UserServiceTest.php
+++ tests/Feature/UserServiceTest.php
@@ -3,8 +3,10 @@
 namespace Tests\Feature;
 
 use App\Services\UserService;
+use Database\Seeders\UserSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
 use function PHPUnit\Framework\assertFalse;
@@ -16,7 +18,7 @@ class UserServiceTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
-
+        DB::delete("delete from users");
         $this->userService = $this->app->make(UserService::class);
     }
 
@@ -27,7 +29,8 @@ class UserServiceTest extends TestCase
 
     public function testLoginSuccess()
     {
-        self::assertTrue($this->userService->login("samuel", "rahasia"));
+        $this->seed([UserSeeder::class]);
+        self::assertTrue($this->userService->login("samuel@localhost", "rahasia"));
     }
 
     public function testLoginUserNotFound()
